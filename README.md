6c14a372832707634d5d59e309039123f7aef889
Israel Deorce <israel.idvj@gmail.com>
2021-02-22 22:53:26 -0300

    wip continuing to draw lines for portfolio page

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # kanever-ui
 Kanever UI
 
+[Check the webpage](https://israeldeorce.github.io/kanever-ui/)
+
 Design on [Figma](https://www.figma.com/file/J4f2Xycbtxi6a7a7ac3KBV/Untitled?node-id=0%3A3)
 
 Created by Israel Deorce
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,13 +1,45 @@
 import ExternalVideos from '../components/ExternalVideos'
+import Polygon from '../components/Polygon'
 import './Portfolio.scss'
 
 function Portfolio() {
   return (
     <section id='portfolio' className='portfolio'>
-      <div className='content-wrap'>
+      <div><span /><span /></div>
+      <div className='title'>
         <h1>PORTF&Oacute;LIO</h1>
-        <ExternalVideos />
+        <Polygon isSideSquare />
+        <span />
       </div>
+      <div className='subtitle'>
+        VIDEOS&nbsp;/&nbsp;AUDIOVISUAL
+        <Polygon isSideSquare />
+      </div>
+      <div className='video-filter'>
+        <span>FILTRO</span><span />
+        <ul>
+          <li>
+            <span />
+            <Polygon color='#DCDBD6' />
+            MOTION GRAPHICS
+          </li>
+          <li>
+            <span />
+            <Polygon color='#DCDBD6' />
+            WHITEBOARD
+          </li>
+          <li>
+            <span />
+            <Polygon color='#DCDBD6' />
+            MOTION GRAPHICS
+          </li>
+        </ul>
+        <div className='portfolio-description'>
+          <p>Confira alguns dos melhores e mais recentes trabalhos produzidos por mim, com parcerias e participações de outros profissionais e produtoras</p>
+        </div>
+      </div>
+      <span/>
+      <ExternalVideos />
     </section>
   )
 }
